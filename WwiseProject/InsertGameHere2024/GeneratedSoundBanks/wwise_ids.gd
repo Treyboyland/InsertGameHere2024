4d017487b6b174fa058559ed0900d0bb74b87dfc
Permadeaf <101829691+Permadeaf@users.noreply.github.com>
2024-02-08 20:04:45 -0600

    Set basic Wwise and Godot settings to make integration run, created test soundbank and test event/sfx

---
+++ WwiseProject/InsertGameHere2024/GeneratedSoundBanks/wwise_ids.gd
@@ -0,0 +1,62 @@
+class_name AK
+
+class EVENTS:
+
+	const PLAYTESTSOUND = 2260221316
+
+	const _dict = {
+		"PlayTestSound": PLAYTESTSOUND
+	}
+
+class STATES:
+
+	const _dict = {}
+
+class SWITCHES:
+
+	const _dict = {}
+
+class GAME_PARAMETERS:
+
+	const _dict = {}
+
+class TRIGGERS:
+
+	const _dict = {}
+
+class BANKS:
+
+	const INIT = 1355168291
+	const TESTBANK = 3291379323
+
+	const _dict = {
+		"Init": INIT,
+		"TestBank": TESTBANK
+	}
+
+class BUSSES:
+
+	const MASTER_AUDIO_BUS = 3803692087
+
+	const _dict = {
+		"Master Audio Bus": MASTER_AUDIO_BUS
+	}
+
+class AUX_BUSSES:
+
+	const _dict = {}
+
+class AUDIO_DEVICES:
+
+	const NO_OUTPUT = 2317455096
+	const SYSTEM = 3859886410
+
+	const _dict = {
+		"No_Output": NO_OUTPUT,
+		"System": SYSTEM
+	}
+
+class EXTERNAL_SOURCES:
+
+	const _dict = {}
+
