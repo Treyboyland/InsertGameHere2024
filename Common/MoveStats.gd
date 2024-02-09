4897ed28c38884c80c66b8520dfbe091a4e5dffe
Ryan Malm <ryan.malm@gmail.com>
2024-02-09 14:02:59 -0600

    moved wwise event to parent node, renamed it sampleGong.  now also plays when player jumps, via event.post_event().  Prettty simple!

--- Common/MoveStats.gd
+++ Common/MoveStats.gd
@@ -3,8 +3,8 @@ extends Resource
 
 @export var move_speed:float = 100
 @export var gravity:float = 10
-@export var friction:float = 0.90
+@export var friction:float = 0.80
 
-@export var jump_power:float = 800
+@export var jump_power:float = 400
 
 
