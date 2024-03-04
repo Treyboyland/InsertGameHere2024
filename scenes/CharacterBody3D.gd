0f98c312286b7e8c865bb4eb4d9c59f300be28a4
Ryan Malm <ryan.malm@us.af.mil>
2024-03-04 15:40:50 -0600

    limited player movement to x y plane, fiddled with 2.5d setup a bit

--- scenes/CharacterBody3D.gd
+++ scenes/CharacterBody3D.gd
@@ -2,7 +2,7 @@ extends CharacterBody3D
 
 
 const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+@export var JUMP_VELOCITY = 4.5
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
@@ -23,9 +23,9 @@ func _physics_process(delta):
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		#velocity.z = direction.z * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		#velocity.z = move_toward(velocity.z, 0, SPEED)
 
 	move_and_slide()
