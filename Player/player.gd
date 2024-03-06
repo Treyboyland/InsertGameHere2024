92baab42d5dfdf18d7547de9116643e8128c2ff3
Ryan Malm <ryan.malm@gmail.com>
2024-03-05 21:21:27 -0600

    removing some unused scenes and scripts, tidying up current test level

--- Player/player.gd
+++ Player/player.gd
@@ -1,19 +1,42 @@
-extends CharacterBody2D
+@tool
+class_name Player
+extends CharacterBody3D
 
-@export var move_stats:MoveStats
-@onready var player_lands = $"Wwise Sound Events/PlayerLands"
 
-var just_hit_something:bool = false
+@export var SPEED = 3.0
+@export var JUMP_VELOCITY = 4.5
+@export var user_controlled: bool = true
+@export var alive:bool = true
+@onready var camera_3d = get_node("Camera3D")
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
 func _ready():
 	pass
-	
-func _physics_process(delta):
-	var touching = get_slide_collision_count()
-	if touching and not just_hit_something:
-		just_hit_something = true
-		#player_lands.post_event()
-		
-	if not touching:
-		just_hit_something = false
-	
 
+func _physics_process(delta):
+	if user_controlled:
+		handle_input(delta)
+	# Add the gravity.
+func handle_input(delta):
+	camera_3d.make_current()
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir = Input.get_axis("ui_left", "ui_right")
+	var direction = (transform.basis * Vector3(input_dir, 0, 0)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		#velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		
+	velocity.z = 0;
+	move_and_slide()
--- autoloads/Global.gd
+++ autoloads/Global.gd
@@ -8,6 +8,7 @@ func _ready():
 	pass
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if Input.is_action_just_released("ui_cancel"):
+		get_tree().quit()
 
 
