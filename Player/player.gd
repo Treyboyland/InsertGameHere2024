a567262dfb7909cac3431d3a729154ab220b7a55
Roosevelt Boyland III <treyboyland@aol.com>
2024-06-08 20:18:32 -0500

    Controlling player using created inputs instead of ui inputs.

--- Player/player.gd
+++ Player/player.gd
@@ -25,12 +25,12 @@ func handle_input(delta):
 		velocity.y -= gravity * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("player_jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_axis("ui_left", "ui_right")
+	var input_dir = Input.get_axis("player_left", "player_right")
 	var direction = (transform.basis * Vector3(input_dir, 0, 0)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
